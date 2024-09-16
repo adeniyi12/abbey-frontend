781d42dff5599aa0f5143cf72a2d77c42350d1a2
Timilehin Adeleye <timi.adeleye@babbangona.com>
2024-09-16 14:21:01 +0100

    implemented user relationship

--- src/components/UserRelationships.tsx
+++ src/components/UserRelationships.tsx
@@ -43,7 +43,7 @@ export default function UserRelationships({ friendsData, followingData, follower
         {getActiveData().map((person: any) => (
           <li key={person.email} className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
             <div className="flex min-w-0 gap-x-4">
-              <img alt="" src={person.imageUrl} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
+              <img alt="" src={person.profile_picture} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
               <div className="min-w-0 flex-auto">
                 <p className="text-sm font-semibold leading-6 text-gray-900">
                   <a href={person.href}>
